93d66a606c5cfd10aa2b580c33f03bc5c3f7b964
khaitbek <khaitbek2005@gmail.com>
2023-11-27 00:43:37 +0500

    Fix todo table error: display the todos that are related to current user

--- todo-list-with-auth/src/app/page.tsx
+++ todo-list-with-auth/src/app/page.tsx
@@ -1,14 +1,18 @@
+import { Todo } from "@prisma/client";
+import { getServerSession } from "next-auth";
 import { todoColumns } from "~/columns/todos";
 import { DataTable } from "~/components/data-table";
 import { TypographyH1 } from "~/components/ui/typography";
+import { authOptions } from "~/server/auth";
 import { db } from "~/server/db";
 
-async function getTodos() {
-  return await db.todo.findMany();
+async function getTodos(userId: Todo["createdById"]) {
+  return await db.todo.findMany({ where: { createdById: userId } });
 }
 
 export default async function HomePage() {
-  const todos = await getTodos();
+  const session = await getServerSession(authOptions);
+  const todos = await getTodos(session!.user.id);
   return (
     <>
       <section className="py-12">
